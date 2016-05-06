5e6afdb17c49e62ce9a8126dc6edad22fc6b9028
Jay Robinson <me@j4y.co>
2016-05-06 17:10:26 +0100

    install monolog

--- public/index.php
+++ public/index.php
@@ -5,6 +5,22 @@ require_once __DIR__.'/../common.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+
+// PROVIDERS
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+
+$app['logger'] = $app->share(function () {
+    $log = new Monolog\Logger('name');
+    $log->pushHandler(
+        new Monolog\Handler\StreamHandler('php://stdout', Monolog\Logger::WARNING)
+    );
+    return $log;
+});
+
+
+// CONTROLLERS
+
+// ROUTES
 
 $app->run();
