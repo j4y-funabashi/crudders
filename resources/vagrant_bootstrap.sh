77844320c29ccea45055249e323771533522100b
Jay Robinson <me@j4y.co>
2016-05-06 11:57:14 +0100

    Setup and config nginx and php-fpm on vagrant box

---
+++ resources/vagrant_bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+export APP_ROOT="/srv/crudders"
+export APP_NAME="crudders"
+
+
+sudo locale-gen en_GB.UTF-8
+
+## INSTALL
+sudo apt-get update -qq \
+    && sudo apt-get upgrade -qq -y \
+    && sudo apt-get install -qq -y \
+        nginx \
+        php5-fpm
+
+
+## CONFIG
+sudo rm /etc/nginx/sites-enabled/default;
+cat resources/nginx.conf \
+    | envsubst '$APP_NAME $APP_ROOT' \
+    | sudo tee /etc/nginx/sites-enabled/$APP_NAME > /dev/null
+
+cat resources/phpfpm.conf \
+    | envsubst '$APP_NAME $APP_ROOT' \
+    | sudo tee /etc/php5/fpm/pool.d/$APP_NAME.conf > /dev/null
+
+
+## RESTART SERVICES
+sudo service nginx restart
+sudo service php5-fpm restart
