7847aa1d08dcd8c7a74ce520d6bf8ef0714c4132
Jay Robinson <me@j4y.co>
2016-05-06 14:46:28 +0100

    Register ServiceControllerServiceProvider

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-print "hello!";
+require_once __DIR__.'/../common.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+$app->run();
