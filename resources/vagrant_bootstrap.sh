775a333c13821a265611eec2f46c3b2ca01fa657
Jay Robinson <me@j4y.co>
2016-10-20 12:13:15 +0100

    Update to ubuntu/xenial64 and php7.0

--- resources/vagrant_bootstrap.sh
+++ resources/vagrant_bootstrap.sh
@@ -3,7 +3,6 @@
 export APP_ROOT="/srv/app"
 export APP_NAME="app"
 
-
 sudo locale-gen en_GB.UTF-8
 
 ## INSTALL
@@ -11,8 +10,8 @@ sudo apt-get update -qq \
     && sudo apt-get upgrade -qq -y \
     && sudo apt-get install -qq -y \
         nginx \
-        php5-fpm
-
+        php7.0-cli \
+        php7.0-fpm
 
 ## CONFIG
 cat $APP_ROOT/env.example \
@@ -20,15 +19,19 @@ cat $APP_ROOT/env.example \
     | sudo tee /srv/.env > /dev/null
 
 sudo rm /etc/nginx/sites-enabled/default;
+
 cat $APP_ROOT/resources/nginx.conf \
     | envsubst '$APP_NAME $APP_ROOT' \
     | sudo tee /etc/nginx/sites-enabled/$APP_NAME > /dev/null
 
 cat $APP_ROOT/resources/phpfpm.conf \
     | envsubst '$APP_NAME $APP_ROOT' \
-    | sudo tee /etc/php5/fpm/pool.d/$APP_NAME.conf > /dev/null
+    | sudo tee /etc/php/7.0/fpm/pool.d/$APP_NAME.conf > /dev/null
 
+## COMPOSER
+curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/bin/composer
+composer install
 
 ## RESTART SERVICES
 sudo service nginx restart
-sudo service php5-fpm restart
+sudo service php7.0-fpm restart
